f47168a166147f7994a21ba3f34ddf362311f4e1
Thabile Sibisi <tzinhle68@gmail.com>
2025-08-25 00:23:34 +0200

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,22 @@
+x, y, z =input("Expression: ").split(" ")
+
+if y == "+":
+    answer = float(1) + float(1)
+    print(answer)
+elif y == "-":
+    answer = float(2) - float(3)
+    print(answer)
+elif y == "*":
+    answer = float(2) * float(2)
+    print(answer)
+elif y == "/":
+    answer = float(50) / float(5)
+    print(answer)
+elif y == "/":
+    answer = (3 / 2)
+    print(answer)
+
+
+
+
+
