500c4850f52d4a8a5c23ca585f4b8238c06f207f
Thabile Sibisi <tzinhle68@gmail.com>
2025-08-25 00:25:47 +0200

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,9 @@
+Greeting = input("Greeting: ")
+
+if Greeting == "Hello" or Greeting == " Hello " or Greeting == "Hello, Newman":
+    print("$0")
+elif Greeting == "How you doing?":
+    print("$20")
+elif Greeting == "What's happening?" or Greeting == "What's up?":
+    print(" $100 ")
+
