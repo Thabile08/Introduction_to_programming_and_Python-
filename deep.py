bbba9db19eb9c9b2e0e73aea29e8571216a813d1
Thabile Sibisi <tzinhle68@gmail.com>
2025-08-25 00:24:58 +0200

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,17 @@
+#Answer = "What is the answer to the great Question of Life,Universe and Everything? "
+Question = input("What is the answer to the great Question of Life,Universe and Everything? ")
+
+if Question == "42":
+    print("Yes")
+elif Question == " 42 ":
+    print("Yes")
+elif Question == "forty two":
+    print("Yes")
+elif Question == "forty-two":
+    print("Yes")
+elif Question == "FoRty TwO":
+    print("Yes")
+else:
+    print("No")
+
+
