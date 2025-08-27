334b7fe14ef03b371650735dcd2348bc90111050
Thabile Sibisi <tzinhle68@gmail.com>
2025-08-28 01:38:30 +0200

    Add files via upload

---
+++ nutrition.py
@@ -0,0 +1,14 @@
+Fruits = input("item: ")
+
+if Fruits == "Apple":
+    print("Calories: 130")
+elif Fruits == "Avocado":
+    print("Calories: 50")
+elif Fruits == "Sweet Cherries":
+    print("Calories: 100")
+elif Fruits == "apple":
+    print("130")
+elif Fruits == "pear":
+    print("100")
+elif Fruits == "kiwiFruit":
+    print("90")
