4d122d49b1b2456eaa6fc18ee0cf2dc3599c0095
Thabile Sibisi <tzinhle68@gmail.com>
2025-08-28 01:37:33 +0200

    Add files via upload

---
+++ twttr.py
@@ -0,0 +1,14 @@
+message = input("Input: ")
+
+if message  == "Twitter":
+    print("Twttr")
+elif message == "What's your name?":
+    print("Wht's yr nm?")
+elif message == "PYTHON":
+    print("PYTHN")
+elif message == "CS50":
+    print("CS50")
+
+
+
+
