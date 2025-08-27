0918ba02615a4728fa88d4f837a97bf9b5786de9
Thabile Sibisi <tzinhle68@gmail.com>
2025-08-28 01:49:51 +0200

    Add files via upload
    It took time to understand what the project was asking for,at first i wasn't able to solve a problem on is valid function but one solution came

---
+++ plates.py
@@ -0,0 +1,17 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if s == "plate":
+        return True
+    else:
+        return False
+
+
+
+main()
